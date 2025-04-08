7abd7582ba00857cc2b3600cfda33931211b57f3
Mohanish Desale <mohanishdesale@gmail.com>
2025-04-08 22:29:41 +0530

    Added Validators to form fields

--- frontend/lib/features/pages/signup_page.dart
+++ frontend/lib/features/pages/signup_page.dart
@@ -11,6 +11,10 @@ class _SignupPageState extends State<SignupPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final emailRegex = RegExp(
+    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  );
   @override
   void dispose() {
     emailController.dispose();
@@ -24,50 +28,75 @@ class _SignupPageState extends State<SignupPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Sign Up.",
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            TextFormField(
-              controller: emailController,
-              decoration: const InputDecoration(hintText: "Name"),
-            ),
-            const SizedBox(height: 15),
-            TextFormField(
-              controller: passwordController,
-              decoration: const InputDecoration(hintText: "Email"),
-            ),
-            const SizedBox(height: 15),
-            TextFormField(
-              controller: nameController,
-              decoration: const InputDecoration(hintText: "Password"),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text(
-                "SIGN UP",
-                style: TextStyle(color: Colors.white, fontSize: 16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sign Up.",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                text: 'Already have an account? ',
-                style: Theme.of(context).textTheme.titleMedium,
-                children: [
-                  TextSpan(
-                    text: 'Sign In',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
+              const SizedBox(height: 30),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: "Name"),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "Name field is required";
+                  }
+                  return null;
+                },
               ),
-            ),
-          ],
+              const SizedBox(height: 15),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(hintText: "Email"),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "Email field is required";
+                  } else if (!emailRegex.hasMatch(value)) {
+                    return "Email is not valid";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(hintText: "Password"),
+                validator: (value) {
+                  if (value == null ||
+                      value.trim().isEmpty ||
+                      value.trim().length < 7) {
+                    return "Password field is invalid!";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "SIGN UP",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Already have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
