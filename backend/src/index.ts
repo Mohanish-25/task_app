082e9fdfeddfc9fb276ee19223d64643b52a9815
Mohanish Desale <mohanishdesale@gmail.com>
2025-04-10 15:04:41 +0530

    created authroute

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import authRouter from "./routes/auth";
 
 const app = express();
 
-app.get("/", (req,res) =>{
-    res.send("Welcome to my app!!!")
-})
+app.use(express.json());
+app.use("/auth", authRouter);
 
-app.listen(8000,()=>{
-    console.log("Server started on port 8000");
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my app");
+});
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
